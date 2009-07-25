53f45d0d022a72f6fac6f26f90606bc08729bfe7
Patrick Crowley <patrick@mokolabs.com>
2009-07-24 18:24:34 -0700

    tweaking search a bit

--- attendees.rb
+++ attendees.rb
@@ -3,6 +3,6 @@
 require 'rubygems'
 require 'twitter'
 
-Twitter::Search.new('Just signed up for @ignitesandiego').each do |r|
+Twitter::Search.new('signed up for @ignitesandiego').each do |r|
   puts "<span class=\"attendee vcard\">@<a href=\"http://twitter.com/#{r.from_user}\" class=\"url fn nickname\">#{r.from_user}</a></span>"
 end
