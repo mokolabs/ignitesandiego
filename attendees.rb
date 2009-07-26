45f619087a4b371417d917faa217576d995f72dc
Edward O'Connor <hober0@gmail.com>
2009-07-26 13:37:20 -0700

    Get as many results on this page as possible.

--- attendees.rb
+++ attendees.rb
@@ -3,6 +3,6 @@
 require 'rubygems'
 require 'twitter'
 
-Twitter::Search.new('signed up for @ignitesandiego').each do |r|
+Twitter::Search.new('signed up for @ignitesandiego').per_page(100).each do |r|
   puts "<span class=\"attendee vcard\">@<a href=\"http://twitter.com/#{r.from_user}\" class=\"url fn nickname\">#{r.from_user}</a></span>"
 end
