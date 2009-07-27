0c346f3c216bb522a2cea10119223764f7beb949
Patrick Crowley <patrick@mokolabs.com>
2009-07-27 12:06:11 -0700

    updated twitter signup process to cache attendees

--- attendees.rb
+++ attendees.rb
@@ -1,8 +1,31 @@
 #!/usr/bin/ruby
-
 require 'rubygems'
 require 'twitter'
+require 'yaml'
 
+# Load cached attendees
+attendees = Array.new
+File.open('attendees.yml') do |file|
+  attendees = YAML::load(file)
+end
+
+# Grab new attendees
 Twitter::Search.new('signed up for @ignitesandiego').per_page(100).each do |r|
-  puts "<span class=\"attendee vcard\">@<a href=\"http://twitter.com/#{r.from_user}\" class=\"url fn nickname\">#{r.from_user}</a></span>"
+  attendees << r.from_user
+end
+
+# Remove duplicates
+attendees = attendees.uniq
+
+# Update cached attendees
+File.open('attendees.yml', 'w') do |file|
+  YAML.dump(attendees, file)
+end
+
+# Update include
+cache = File.new('attendees.inc', "w+")
+attendees.each do |attendee|
+  cache.puts "<span class=\"attendee vcard\">" + 
+             "@<a href=\"http://twitter.com/#{attendee}\" class=\"url fn nickname\">" + 
+             "#{attendee}</a></span>"
 end
