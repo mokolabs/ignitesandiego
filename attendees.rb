f0e4730c19ba0cc486ad86dc4f6f3f3fb848ec59
Patrick Crowley <patrick@mokolabs.com>
2009-07-27 12:18:08 -0700

    added code to create cache files (if they are missing)

--- attendees.rb
+++ attendees.rb
@@ -3,10 +3,21 @@ require 'rubygems'
 require 'twitter'
 require 'yaml'
 
-# Load cached attendees
+# Let's get this party started!
 attendees = Array.new
-File.open('attendees.yml') do |file|
-  attendees = YAML::load(file)
+
+# Create cached files (if missing)
+['yml', 'inc'].each do |type|
+  unless File.exists?("attendees.#{type}")
+    File.new("attendees.#{type}",  "w+")
+  end
+end
+
+# Load cached attendees
+unless File.zero?('attendees.yml')
+  File.open('attendees.yml') do |file|
+    attendees = YAML::load(file)
+  end
 end
 
 # Grab new attendees
@@ -22,7 +33,7 @@ File.open('attendees.yml', 'w') do |file|
   YAML.dump(attendees, file)
 end
 
-# Update include
+# Update include file
 cache = File.new('attendees.inc', "w+")
 attendees.each do |attendee|
   cache.puts "<span class=\"attendee vcard\">" + 
